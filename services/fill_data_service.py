8f04b508f6f26a74525b8296001a8ebbb4feccfe
Nick Laert <nick.laert@digitalhub.man>
2021-04-11 16:41:07 +0100

    add products to cart

--- services/fill_data_service.py
+++ services/fill_data_service.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 import time
+import re
 
 
 class FillDataService:
@@ -26,8 +27,11 @@ class FillDataService:
         self.__wait_for_load('customers_button').click()
         # Wait for element does not work here because this is a div and not a button / link
         time.sleep(5)
-        if self.__check_if_customer_exists(order['customer_id']):
+        customer_row = self.__check_if_customer_exists(order['customer_id'])
+        if customer_row is None:
             self.__create_customer(order)
+            customer_row = self.__check_if_customer_exists(order['customer_id'])
+        self.__create_order(order, customer_row)
 
     def __create_customer(self, order):
         self.driver.find_element_by_id('new_customer').click()
@@ -55,8 +59,25 @@ class FillDataService:
             ref = int(row.text[0:firstSpaceIndex])
             id = row.find_element_by_id('portfolio_ref_' + str(ref)).find_element_by_tag_name('span').text
             if int(id) == customer_id:
-                return True
-        return False
+                return row
+        return None
+
+    def __create_order(self, order, customer_row):
+        customer_row.find_element_by_class_name('fa-shopping-cart').click()
+        for item in order['line_items']:
+            self.__add_products(item)
+
+    def __add_products(self, item):
+        sku = self.__get_complete_sku(item['sku'])
+        self.__wait_for_load_by_xpath('//*[@id="block_0"]/div[2]/span/table/thead/tr[1]/td/div/input[1]').send_keys(sku)
+        self.driver.find_element_by_xpath('//*[@id="block_0"]/div[2]/span/table/thead/tr[1]/td/div/input[2]').send_keys(item['quantity'])
+        time.sleep(2)
+        self.__wait_for_load_by_css('.add_item_results').click()
+        self.driver.find_element_by_class_name('fa-cloud').click()
+
+    def __get_complete_sku(self, sku):
+        index = re.search(r'\d', sku).start()
+        return sku[0: index] + '-' + sku[index: len(sku)]
 
     def __wait_for_load(self, element_id):
         return WebDriverWait(self.driver, self.config['timeout'])\
