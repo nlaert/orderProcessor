5a883d691bdacb68f84e488e15c1032153fcc743
Nick Laert <nick.laert@digitalhub.man>
2021-04-11 16:46:08 +0100

    renaming env vars

--- config/read_config.py
+++ config/read_config.py
@@ -13,11 +13,11 @@ class ReadConfig:
         return self.config
 
     def __read_from_env(self):
-        self.config['dropshipping_url'] = environ.get('dropshipping_url')
-        self.config['dropshipping_login'] = environ.get('dropshipping_login')
-        self.config['dropshipping_pass'] = environ.get('dropshipping_pass')
-        self.config['store_url'] = environ.get('store_url')
+        self.config['dropshipping_url'] = environ.get('dropshipping.url')
+        self.config['dropshipping_login'] = environ.get('dropshipping.login')
+        self.config['dropshipping_pass'] = environ.get('dropshipping.pass')
+        self.config['store_url'] = environ.get('store.url')
         self.config['headless'] = environ.get('headless', True) == 'True'
         self.config['timeout'] = int(environ.get('timeout'))
-        self.config['firefox_binary'] = environ.get('firefox_binary')
+        self.config['firefox_binary'] = environ.get('firefox.binary')
         self.config['geckodriver'] = environ.get('geckodriver')
