4b1059dad48b3a38ba29a14b068ee3eb1838e02b
Nick Laert <nick.laert@digitalhub.man>
2021-04-11 15:20:27 +0100

    check if customer exists

--- services/fill_data_service.py
+++ services/fill_data_service.py
@@ -26,8 +26,37 @@ class FillDataService:
         self.__wait_for_load('customers_button').click()
         # Wait for element does not work here because this is a div and not a button / link
         time.sleep(5)
+        if self.__check_if_customer_exists(order['customer_id']):
+            self.__create_customer(order)
+
+    def __create_customer(self, order):
         self.driver.find_element_by_id('new_customer').click()
-        self.__fill_ds_customer(order)
+        self.__wait_for_load_by_name('client_reference').send_keys(order['customer_id'])
+        self.driver.find_element_by_name('name').send_keys(order['shipping']['first_name'] + ' '
+                                                           + order['shipping']['last_name'])
+        self.driver.find_element_by_name('organization').send_keys(order['shipping']['company'])
+        self.driver.find_element_by_name('email').send_keys(order['billing']['email'])
+        self.driver.find_element_by_name('tel').send_keys(order['billing']['phone'])
+        self.driver.find_element_by_name('addressLine1').send_keys(order['shipping']['address_1'])
+        self.driver.find_element_by_name('addressLine2').send_keys(order['shipping']['address_2'])
+        self.driver.find_element_by_name('postalCode').send_keys(order['shipping']['postcode'])
+        self.driver.find_element_by_name('locality').send_keys(order['shipping']['city'])
+        self.driver.find_element_by_name('administrativeArea').send_keys(order['shipping']['city'])
+        dropdown = Select(self.driver.find_element_by_id('country_select'))
+        dropdown.select_by_value(order['shipping']['country'])
+
+        self.driver.find_element_by_id('save_new_client_button').click()
+        self.__wait_for_load_by_css('button.ui-button:nth-child(1)').click()
+
+    def __check_if_customer_exists(self, customer_id):
+        rows = self.driver.find_elements_by_css_selector('#table > table > tbody > tr')
+        for row in rows:
+            firstSpaceIndex = row.text.index(' ')
+            ref = int(row.text[0:firstSpaceIndex])
+            id = row.find_element_by_id('portfolio_ref_' + str(ref)).find_element_by_tag_name('span').text
+            if int(id) == customer_id:
+                return True
+        return False
 
     def __wait_for_load(self, element_id):
         return WebDriverWait(self.driver, self.config['timeout'])\
@@ -44,21 +73,3 @@ class FillDataService:
     def __wait_for_load_by_css(self, selector):
         return WebDriverWait(self.driver, self.config['timeout']) \
             .until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
-
-    def __fill_ds_customer(self, order):
-        self.__wait_for_load_by_name('client_reference').send_keys(order['customer_id'])
-        self.driver.find_element_by_name('name').send_keys(order['shipping']['first_name'] + ' '
-                                                           + order['shipping']['last_name'])
-        self.driver.find_element_by_name('organization').send_keys(order['shipping']['company'])
-        self.driver.find_element_by_name('email').send_keys(order['billing']['email'])
-        self.driver.find_element_by_name('tel').send_keys(order['billing']['phone'])
-        self.driver.find_element_by_name('addressLine1').send_keys(order['shipping']['address_1'])
-        self.driver.find_element_by_name('addressLine2').send_keys(order['shipping']['address_2'])
-        self.driver.find_element_by_name('postalCode').send_keys(order['shipping']['postcode'])
-        self.driver.find_element_by_name('locality').send_keys(order['shipping']['city'])
-        self.driver.find_element_by_name('administrativeArea').send_keys(order['shipping']['city'])
-        dropdown = Select(self.driver.find_element_by_id('country_select'))
-        dropdown.select_by_value(order['shipping']['country'])
-
-        self.driver.find_element_by_id('save_new_client_button').click()
-        self.wait_for_load_by_css('button.ui-button:nth-child(1)').click()
