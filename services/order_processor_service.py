ef9c620f578589d4145ed31f97564c5758652346
Nick Laert <nlaert@gmail.com>
2021-12-12 12:29:45 +0000

    Avoid creating duplicated invoices

--- services/order_processor_service.py
+++ services/order_processor_service.py
@@ -14,7 +14,13 @@ class OrderProcessorService:
             thread = Thread(target=self.dropshipping_service.fill_dropshipping, args=(order,))
             thread.daemon = True
             thread.start()
-        elif order['status'] == 'completed':
+        elif order['status'] == 'completed' and self.__should_create_invoice(order):
             thread = Thread(target=self.invoice_service.fill_invoice, args=(order,))
             thread.daemon = True
             thread.start()
+
+    def __should_create_invoice(self, order):
+        for meta_data in order['meta_data']:
+            if meta_data['key'] == 'my_custom_val':
+                return False
+        return True
